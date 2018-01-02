45ae3debea5a108ad0f933113f7545b98a1f7df1
lrgeorge27 <lrgeorge27@gmail.com>
2018-01-02 00:57:23 +0000

    node.js 1.2 - 1.7

--- node.js
+++ node.js
@@ -27,3 +27,70 @@ http.createServer(function(request, response) { //request event
         response.end();
     }, 5000); //5000ms = 5 seconds
 }).listen(8080);
+
+//1.2 Hello You
+var http = require('http');
+
+http.createServer(function(request, response) {
+    response.writeHead(200);
+    response.write("Hello, this is Lauren");
+    response.end();
+}).listen(8080);
+
+//1.3 Convert Blocking
+//blocking function readFileSync. 
+var fs = require('fs');
+var contents = fs.readFileSync('index.html');
+console.log(contents);
+
+//Convert the code to be non-blocking using the readFile function instead.
+fs.readFile('index.html', function(error, contents) {
+    console.log(contents);
+}); //removed Sync from readFile, added a callback function with error and contents parameters, moved console.log() inside function.
+
+//1.4 Running code
+
+// 1.5 Read File in Server 140 PTS
+// Now that you know how to create an HTTP server and how to read a file off the filesystem in a non-blocking way, let's try to combine the two.
+// Instead of just writing a string to the HTTP response, write the contents of index.html to the response instead.
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(request, response) {
+    response.writeHead(200);
+    fs.readFile('index.html', function(error, contents) { //After response.writeHead(200), add a call to fs.readFile() that reads index.html asynchronously. Remember to pass a callback function, that accepts an error parameter, and a contents parameter.
+        response.write(contents); //Now that you have the file contents, write it to the response.
+        response.end(); //move end inside the callback function
+    });
+
+}).listen(8080);
+
+// 1.6 Issuing a Request 150 PTS
+// Let's see our new server in action. We've already run node app.js, so in the terminal below use curl to issue a request to http://localhost:8080 and we'll see our server respond with the contents of index.html.
+// //In the console:
+// <html><p>Hello, this is Dog</p></html>
+// Congratulations, you're correct!
+
+// 1.7 Writing Response Headers 150 PTS
+// Up until now all we've been sending into the response.writeHead() function is the status code. However, it can take additional parameters.
+http.createServer(function(request, response) {
+    response.writeHead(200, {
+        'Content-Type': 'text/html' //code I added based on node documentation https://nodejs.org/api/http.html#http_response_writehead_statuscode_statusmessage_headers
+    });
+
+    fs.readFile('index.html', function(err, contents) {
+        response.write(contents);
+        response.end();
+    });
+
+}).listen(8080);
+
+// 1.8 Response End 250 PTS
+// Our original Hello server can be shortened since the response.end() function optionally takes data as a parameter. Remove the response.write line altogether, and send the hello string as a parameter on the response.end function. This will send the data, and once finished add the end to the response.
+var http = require('http');
+
+http.createServer(function(request, response) {
+    response.writeHead(200);
+    response.write("Hello, this is dog");
+    response.end();
+}).listen(8080);
