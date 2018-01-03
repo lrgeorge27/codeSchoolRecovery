841b55d271d747cc6bfa166e002a6055ae24fac4
lrgeorge27 <lrgeorge27@gmail.com>
2018-01-03 02:18:51 +0000

    finished Node.js level 4

--- node4.js
+++ node4.js
@@ -115,3 +115,88 @@ my_app / package.json {
 "connect": "~1.8" //fetches ranges >= 1.8.0 <1.9.0, api could change
 "connect": "~1.8.7" //fetches ranges >= 1.8.7 <1.9.0, can be dangerous
 http: //semver.org/
+
+    // 4.2 Missing Exports 150 PTS
+    // Notice the two different files: high_five.js on the left side and app.js on the right. The code as it's written will not work, high_five.js isn't exporting anything.
+    // Add the proper exports line to have a successful high five!
+    var highfive = function() {
+        console.log("smack!!");
+    };
+module.exports = highfive;
+
+// 4.3 Export A Function 140 PTS
+// Notice the app.js file with the myRequest function below. Let's refactor myRequest out to its own my_request.js module.
+// Move the myRequest function and the http require into my_request.js
+
+//moved to my_request.js file
+var http = require('http');
+
+var myRequest = function(message) {
+    var request = http.request('http://codeschool.com', function(response) {
+        response.pipe(process.stdout, { end: false });
+    });
+
+    request.write(message);
+    request.end();
+};
+//Export the myRequest function.
+module.exports = myRequest;
+
+//Require the my_request.js module in app.js.
+//app.js
+var myRequest = require('my_request.js');
+
+myRequest('Hello, this is dog.');
+
+// 4.4 Exporting An Object 150 PTS
+// The app.js code on the right side does not work. 
+// Once again we forgot to export our functions.
+// In the logger.js file, export the info function so we can use it in app.js by assigning it to the exports object.
+// In the logger.js file, export the warn function so we can use it in app.js by assigning it to the exports object.
+// In the logger.js file, export the error function so we can use it in app.js by assigning it to the exports object.
+exports.warn = function(message) {
+    console.log("Warning: " + message);
+};
+
+exports.info = function(message) {
+    console.log("Info: " + message);
+};
+
+exports.error = function(message) {
+    console.log("Error: " + message);
+};
+
+// 4.5 Installing Local Modules 150 PTS
+// Practice using npm by installing the npm module underscore using the npm install command.
+$ npm install underscore
+
+// 4.6 Installing Global Modules 150 PTS
+// Now install the coffee-script module, but install it globally so you can use the coffee executable that comes with coffee-script.
+$ npm install coffe - script - g
+
+// 4.7 Dependency 240 PTS
+// Add two dependencies to our package.json file, connect and underscore. 
+// We'll want to use version 2.1.1 of connect and version 1.3.3 of underscore.
+// Add the connect dependency to package.json
+// Add the underscore dependency to package.json
+{
+    "name": "My Awesome Node App",
+    "version": "1",
+    "dependencies": {
+        "connect": "2.1.1",
+        "underscore": "1.3.3"
+    }
+}
+
+// 4.8 Semantic Versioning 250 PTS
+// We want to make sure we are always up-to-date with the most recent patch-level changes to our dependencies when we run npm install.
+// Update the connect version on package.json to fetch the latest patch-level changes. All we have to do is add one character to the beginning of the version number.
+// Now update the underscore version on package.json to fetch the latest patch-level changes. Again, all we have to do is add one character to the beginning of the version number.
+{
+    "name": "My Awesome Node App",
+    "version": "1",
+    "dependencies": {
+        "connect": "~2.2.1",
+        "underscore": "~1.3.3"
+    }
+}
