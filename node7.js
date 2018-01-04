7815d5b89d09a79a8cfd23e3ca2ffb101025f33f
lrgeorge27 <lrgeorge27@gmail.com>
2018-01-04 18:19:34 +0000

    Finished Node.js!

--- node7.js
+++ node7.js
@@ -151,3 +151,120 @@ server.on('remove chatter', function(name){
 });
 }
 </script>
+
+// 7.2 Simple Redis Commands 130 PTS
+// Let's start practicing using the redis key-value store from our node application.
+
+var redis = require('redis'); // Require the redis module and assign it to a variable called redis.
+var client = redis.createClient(); // Create a redis client and assign it to a variable called client.
+client.set("name", "Lauren"); // On the client, set the name property to your name.
+
+// 7.3 Get A Key 240 PTS
+// We have already stored a value in the question key. Use the redis client to issue a get command to redis to retrieve and then log the value.
+
+client.get('question', function(err, data) { // Use the redis client to issue a get command using the 'question' key to retrieve a value. Remember, the get function takes a callback which expects two arguments, error and data.
+    console.log(data); // Log the value retrieved with console.log.
+});
+
+// 7.4 Working With Lists 1 240 PTS
+// As we saw in the video, redis can do more than just simple key-value pairs. We are going to be using redis' LISTS later to add persistence to our live-moderation app, so let's practice using them now.
+
+client.lpush('questions', question1, function(err, value) { // Using the redis client's lpush command, insert question1 into the questions list. Then, console.log the result you receive. Remember, the lpush function takes a callback as its last argument, which expects an error and value to be passed as arguments.
+    console.log(value);
+});
+
+client.lpush('questions', question2, function(err, value) { // Using the redis client's lpush command, insert question2 into the questions list. Then console.log the result you receive.
+    console.log(value);
+});
+
+// 7.5 Working With Lists 2 230 PTS
+// Now that we have seeded the questions list, use the lrange() command to return all of the items and log them.
+client.lrange('questions', 0, -1, function(err, value) { // Use the lrange() command to return all of the items from the questions key.
+    console.log(value); // Now that we have called lrange(), use console.log to log the result from redis.
+});
+
+// 7.6 Persisting Questions 240 PTS
+// Let's go back to our live-moderation app and add some persistence, first to the questions people ask.
+
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var socket = require('socket.io');
+var io = socket.listen(server);
+
+var redis = require('redis');
+var redisClient = redis.createClient();
+
+io.sockets.on('connection', function(client) {
+    client.on('answer', function(question, answer) {
+        client.broadcast.emit('answer', question, answer);
+    });
+
+    client.on('question', function(question) {
+        if (!client.question_asked) {
+            client.question_asked = true;
+            client.broadcast.emit('question', question);
+            // add the question to the list here
+            redisClient.lpush('questions', question); // Use the lpush command to add new questions to the list named questions. Do this inside the listener for the 'question' event.
+        }
+    });
+});
+
+
+// 7.7 Emitting Stored Questions 230 PTS
+// Now that we have questions stored in redis, let's emit them whenever a new client connects to the server through socket.io.
+var redis = require('redis');
+var redisClient = redis.createClient();
+
+io.sockets.on('connection', function(client) {
+    redisClient.lrange('questions', 0, -1, function(err, questions) { // Use the lrange command to retrieve a list of questions that represent the questions list within redis.
+        questions.forEach(function(question) {
+            client.emit('question', question); // Inside of the lrange callback, use a forEach loop to iterate through the questions and emit() each question to the client. Remember, don't use broadcast.emit because we only want to send the questions to the client that is connecting to the server.
+        });
+    });
+    client.on('answer', function(question, answer) {
+        client.broadcast.emit('answer', question, answer);
+    });
+
+    client.on('question', function(question) {
+        if (!client.question_asked) {
+            client.question_asked = true;
+            client.broadcast.emit('question', question);
+            redisClient.lpush("questions", question);
+        }
+    });
+});
+
+
+// 7.8 Limiting Questions Stored 240 PTS
+// Great work! One last thing though, since every time a new question comes in we store it in the questions list, we might run into a problem where there are just too many questions stored in that list.
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+var redis = require('redis');
+var redisClient = redis.createClient();
+
+io.sockets.on('connection', function(client) {
+    redisClient.lrange("questions", 0, -1, function(err, questions) {
+        questions.forEach(function(question) {
+            client.emit("question", question);
+        });
+    });
+
+    client.on('answer', function(question, answer) {
+        client.broadcast.emit('answer', question, answer);
+    });
+
+    client.on('question', function(question) {
+        if (!client.question_asked) {
+            client.question_asked = true;
+            client.broadcast.emit('question', question);
+            redisClient.lpush("questions", question, function() { // Add a callback to lpush that will be used to limit the size of the list down to a max of 20.
+                redisClient.ltrim("questions", 0, 19); // Use the ltrim command to limit the size of the list stored within redis to a maximum size of 20.
+            });
+        }
+    });
+
+});
